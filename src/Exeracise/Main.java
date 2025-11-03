667b5d744d19ef68ef2b9f9b5fdce1879a5c521b
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-03 21:03:01 +0530

    first commit

---
+++ src/Exeracise/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int number= sc.nextInt();
+        int OriginalNumber= number;
+        int ReversedNumber=0;
+
+
+        while (number>0){
+            int Remainder=number%10;
+            ReversedNumber=ReversedNumber*10+Remainder;
+            number= number/10;
+        }
+        if(ReversedNumber== OriginalNumber){
+            System.out.println(OriginalNumber+"is a Palindrome");
+        }
+        else{
+            System.out.println(OriginalNumber+"is not a Palindrome");
+        }
+
+    }
+}
