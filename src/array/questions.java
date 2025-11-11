6eae925c66e02f30cc1bae35eb3cb7c42467366c
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-11 21:38:10 +0530

    modified

--- src/array/questions.java
+++ src/array/questions.java
@@ -17,6 +17,23 @@ public class questions {
         System.out.println(Arrays.toString(nums));
     }
 
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum =0;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+
+            if(sum>max){
+                max = sum;
+            }
+
+            if(sum<0){
+                sum =0;
+            }
+        }
+        return max;
+    }
+
 
     public static void nextPermutation(int[] nums) {
         int index =-1;
