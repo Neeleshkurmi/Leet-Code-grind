9ebba7229472993bb44afdbcd2e058451ff9e826
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-18 22:16:13 +0530

    modified

--- src/array/questions.java
+++ src/array/questions.java
@@ -25,6 +25,16 @@ public class questions {
     }
 
 
+    //nov 18
+
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length;
+        int i = 0;
+        while(i < n - 1)
+            i += bits[i] + 1;
+        return i == n - 1;
+    }
+
     //nov 17
 
     public static int evalRPN(String[] tokens) {
