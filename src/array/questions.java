4f6f4d4d65c0b67918dd2e18203aed6348a58b29
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-18 23:32:37 +0530

    modified

--- src/array/questions.java
+++ src/array/questions.java
@@ -37,6 +37,34 @@ public class questions {
 
     //nov 17
 
+    public static int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prevTime =0;
+
+        for(String log : logs){
+            String[] parts = log.split(":");
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int timestamp = Integer.parseInt(parts[2]);
+
+            if(type.equals("start")){
+                if(!stack.isEmpty()){
+                    result[stack.peek()] += (timestamp - prevTime);
+                }
+                stack.push(id);
+                prevTime = timestamp;
+            }
+
+            else{
+                int popId = stack.pop();
+                result[popId] += (timestamp - prevTime + 1);
+                prevTime = timestamp + 1;
+            }
+        }
+        return result;
+    }
+
     public static int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
 
