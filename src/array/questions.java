e46eb679004c97869815a18fb59f7f858a2204a3
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-19 23:16:04 +0530

    modified

--- src/array/questions.java
+++ src/array/questions.java
@@ -27,6 +27,21 @@ public class questions {
 
 
     //nov 19
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int idx = stack.pop();
+                result[idx] = i-idx;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
     public int findFinalValue(int[] nums, int original) {
         Set<Integer> set = new HashSet<>();
 
