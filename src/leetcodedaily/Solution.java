e9be08993a294fa58dff93e32f6042b6cf4c16a3
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-25 22:30:15 +0530

    nov 25

--- src/leetcodedaily/Solution.java
+++ src/leetcodedaily/Solution.java
@@ -4,8 +4,22 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(findMin(new int[]{2,3,4,5,1}));
+        int num = (int)(Math.pow(10,5));
+        System.out.println(smallestRepunitDivByK(23));
+    }
 
+    //nov 25
+    public static int smallestRepunitDivByK(int k) {
+        if(k==1) return 1;
+
+        if(k%2==0 || k%5==0) return -1;
+
+        int rem = 0;
+        for(int i=1;i<=k;i++){
+            rem = (rem*10+1)%k;
+            if(rem==0) return i;
+        }
+        return -1;
     }
 
     //no 22
