d8cc5eb85635792c8044ef619cb21f13c7e769e7
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-29 01:02:37 +0530

    nov 27

--- src/leetcodedaily/Solution.java
+++ src/leetcodedaily/Solution.java
@@ -19,6 +19,33 @@ public class Solution {
 
 
     //nov 28
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+
+        int i = 0;
+        while (i < nums.length) {
+            int start = nums[i];
+            int j = i;
+            // Expand the range as long as elements are consecutive
+            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1) {
+                j++;
+            }
+
+            // Format the range string
+            if (nums[j] == start) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + nums[j]);
+            }
+
+            // Move to the next potential start of a range
+            i = j + 1;
+        }
+        return result;
+    }
     public static int titleToNumber(String columnTitle) {
         int val = 64, ans =0, n = columnTitle.length();
 
