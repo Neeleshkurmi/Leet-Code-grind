c71bf0279f29119af9d5954c588e7d3c111e7f2e
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-29 18:57:41 +0530

    nov 29

--- src/leetcodedaily/Solution.java
+++ src/leetcodedaily/Solution.java
@@ -33,11 +33,12 @@ public class Solution {
         while(i<s.length() && Character.isDigit(s.charAt(i))){
             int digit = s.charAt(i)-'0';
             num = num*10 + digit;
+            if(num<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if(num>Integer.MAX_VALUE) return Integer.MAX_VALUE;
             i++;
         }
 
-        if(num<Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        if(num>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
 
         return (int) (num*sign);
     }
