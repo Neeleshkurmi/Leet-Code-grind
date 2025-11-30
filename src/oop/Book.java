0b0d1dccb27f6d6f47d4412736b7ce96b4bf9157
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-11-30 20:06:32 +0530

    started learning file handling

---
+++ src/oop/Book.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Book {
+    String name;
+    String author;
+    String pDate;
+    int price;
+    int gRanking;
+
+    byte m; int a; double b; char c; float f; boolean n;
+
+    public Book(){}
+
+    public static void main(String[] args) {
+        System.out.println(new Book());
+        System.out.println();
+    }
+}
