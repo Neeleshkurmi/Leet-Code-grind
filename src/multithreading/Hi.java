855d85930e44e1fd1cfcda18d19d06998d58379f
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-05 00:25:48 +0530

    updation

--- src/multithreading/Hi.java
+++ src/multithreading/Hi.java
@@ -59,3 +59,76 @@ class Daemon extends Thread{
         daemon.start();
     }
 }
+
+class Example extends Thread{
+    private static boolean LOCK = false;
+    @Override
+    public void run() {
+        try {
+            this.mySynchronisedBlock();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void mySynchronisedBlock() throws InterruptedException {
+        synchronized (this){
+            for (int i = 0; i <=6; i++) {
+                System.out.println(STR."\{this.getName()} accessing sync block");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.start();
+        Example example1 = new Example();
+        example1.start();
+    }
+}
+
+class Node{
+    Node head;
+    int val;
+    Node next;
+
+    Node(int val){
+        this.val = val;
+    }
+    void insert(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+    }
+    Node getHead(){
+        return head;
+    }
+    void display() {
+        Node temp = head;
+        while (temp!=null){
+            System.out.print(STR."\{temp.val}->");
+            temp = temp.next;
+        }
+        System.out.print("END");
+    }
+    void display(Node temp){
+        while (temp!=null){
+            System.out.print(STR."\{temp.val}->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+    Node reverse(Node temp){
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node node = new Node(-1);
+        node.insert(1);
+        node.insert(2);
+        node.insert(3);
+        node.display();
+        Node temp = node.reverse(node.getHead());
+        node.display(temp);
+    }
+}
