eb7f71ff14df805e8754dfb14a4700f9e076df41
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-06 00:03:25 +0530

    updated on 5th of dec

--- src/array/search/Solution.java
+++ src/array/search/Solution.java
@@ -9,11 +9,39 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(binarySearch(new int[]{},3));
         System.out.println(leader(new int[] {16, 17, 4, 3, 5, 2}));
-        System.out.println(search(new int[]{4,5,6,7,0,1,2},1));
+        System.out.println(search2(new int[]{1,0,1,1,1},0));
     }
 
 
     //dec 5
+    // variation 2 same question
+    public static boolean search2(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end -start)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            //left half is rotated or not
+            else if (nums[start]<=nums[mid]){
+                if(target>=nums[start] && target<=nums[mid]){
+                    end = mid -1;
+                }
+                else {
+                    start = mid +1;
+                }
+            }
+            else {
+                if(target>=nums[mid] && target<=nums[end]){
+                    start = mid+1;
+                }
+                else {
+                    end = mid -1;
+                }
+            }
+        }
+        return false;
+    }
     //search in sorted rotated array
     public static int search(int[] nums, int target){
         int start = 0, end = nums.length-1;
