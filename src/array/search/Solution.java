48fd8c12d65feef7e733232cb9f025e574a124d2
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-06 16:18:29 +0530

    updated on dec 5

--- src/array/search/Solution.java
+++ src/array/search/Solution.java
@@ -8,27 +8,56 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
 
-        System.out.println(binarySearch(new int[]{},3));
-        System.out.println(leader(new int[] {16, 17, 4, 3, 5, 2}));
-        System.out.println(search2(new int[]{1,0,1,1,1},0));
-        System.out.println(findMin(new int[]{2,1}));
+//        System.out.println(binarySearch(new int[]{},3));
+//        System.out.println(leader(new int[] {16, 17, 4, 3, 5, 2}));
+//        System.out.println(search2(new int[]{1,0,1,1,1},0));
+//        System.out.println(findMin(new int[]{2,1}));
+        System.out.println(findMin(new int[]{1,2,3,4,5,6}));
+        System.out.println(findMin(new int[]{8,1,2,3,4,5,6,7}));
+        System.out.println(findMin(new int[]{1,2}));
+        System.out.println(findMin(new int[]{2,0}));
+        System.out.println(findMin(new int[]{6,7,8,1,2,3,4,5}));
     }
 
     //dec 6
-    public static int findMin(int[] nums) {
-        int start = 0, end = nums.length-1;
-
+    public static int findKRotation(int[] arr) {
+//        int count =0;
+//
+//        for(int i=1; i<arr.length; i++){
+//            if(arr[i]<arr[i-1]){
+//                count = i;
+//            }
+//        }
+//        return count;
+        int start =0, end = arr.length-1;
         while(start<end){
-            int mid = start+ (end -start)/2;
-
-            if(nums[mid]>nums[end]){
-                start = mid+1;
-            }
-            else {
+            int mid = start+(end -start)/2;
+            if(arr[start]>arr[mid]){
                 end = mid;
             }
+            else {
+                start =mid+1;
+            }
         }
-        return nums[start];
+        return start;
+    }
+    public static int findMin(int[] nums) {
+        int ans= Integer.MAX_VALUE;
+        int start = 0, end = nums.length-1;
+
+        while(start<=end){
+            int mid = start+ (end -start)/2;
+
+            if(nums[start]<=nums[mid]){
+                ans = Math.min(nums[start], ans);
+                start = mid+1;
+            }
+            else{
+                ans = Math.min(nums[mid], ans);
+                end = mid-1;
+            }
+        }
+        return ans;
     }
 
     //dec 5
