9a08bd2999a8361f1b93db8368b796b8bd274912
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-17 22:43:57 +0530

    upddated on dec 17

--- src/leetcodedaily/Main.java
+++ src/leetcodedaily/Main.java
@@ -1,10 +1,43 @@
 package leetcodedaily;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+    //dec 17
+
+    public static ListNode oddEvenList(ListNode head){
+        if(head==null || head.next==null) return head;
+
+        List<Integer> list = new ArrayList<>();
+
+        ListNode temp = head;
+
+        while(temp!=null && temp.next!=null){
+            list.add(temp.val);
+            temp = temp.next.next;
+        }
+        if(temp!=null) list.add(temp.val);
+        temp = head.next;
+
+        while(temp!=null && temp.next!=null){
+            list.add(temp.val);
+            temp = temp.next.next;
+        }
+        if(temp!=null) list.add(temp.val);
+
+        temp = head;
+        int i=0;
+        while(temp!=null){
+            temp.val= list.get(i++);
+            temp = temp.next;
+        }
+        return head;
 
     }
     //dec 16
@@ -45,4 +78,13 @@ public class Main {
         sb.reverse();
         return sb.toString();
     }
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
 }
