4182b3d84e59b69461f8b19db56136bc2f4f7cf4
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-18 23:51:43 +0530

    upddated on dec 18

--- src/leetcodedaily/Main.java
+++ src/leetcodedaily/Main.java
@@ -9,6 +9,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(wordPattern("abba","dog cat cat "));
     }
+
+    //dec 18
+    public static ListNode swapPairs(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode first = head, second = head.next;
+
+        while(second!=null){
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+
+            first= first.next.next;
+            second= (second.next==null)? null: second.next.next;
+        }
+        return head;
+    }
     //dec 17
 
     public static boolean wordPattern(String pattern, String s) {
