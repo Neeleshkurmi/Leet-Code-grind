aafc76409340e8759f02722870263cfa9d40ec84
neeleshkurmi <neeleshp212@gmail.com>
2026-04-10 23:14:54 +0530

    uncommented database seeding in .yml

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -6,9 +6,18 @@ public class Recursion{
         List<List<String>> list = new ArrayList<>();
 
         // permute("aabaa", 0, "", list);
-        solve("aabaa", 0, new ArrayList<>(), list);
+        // solve("aabaa", 0, new ArrayList<>(), list);
 
-        System.out.println(list);
+        // System.out.println(list);
+
+        System.out.println(isAnagram("abaaca", "caabaa"));
+
+        // System.out.println((int)'b');
+        // System.out.println((int)'c');
+        //     "abca", "caba"
+        // ));
+
+
     }
 
     public static void permute(String s, int index, String ans, List<String> res){
@@ -71,6 +80,54 @@ public class Recursion{
                 solve(s, i+1, temp, res);
                 temp.removeLast();
             }
+        }
+    }
+
+    static boolean isAnagram(String s, String t){
+        if(s.length() != t.length()) return false;
+
+        int[] ch = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            ch[s.charAt(i) - 'a']++;
+            ch[t.charAt(i) - 'a']--;
+        }
+
+        for(int num: ch){
+            if(num!=0) return false;
+        }
+        return true;
+    }
+
+
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(2, "abc");
+        map.put(3, "def");
+        map.put(4, "ghi");
+        map.put(5, "jkl");
+        map.put(6, "mno");
+        map.put(7, "pqrs");
+        map.put(8, "tuv");
+        map.put(9, "wxyz");
+
+        dfs("", 0, digits, res, map);
+        return res;
+    }
+
+    void dfs(String curr, int index, String digits, List<String> res, Map<Integer, String> map){
+        
+        if(digits.length() == index){
+            res.add(curr);
+            return;
+        }
+
+        String letters = map.get(digits.charAt(index) - '0');
+
+        for(char ch : letters.toCharArray()){
+            dfs(curr + ch, index + 1, digits, res, map);
         }
     }
 }
